b0dc88c7f57f0487c63b85ddaa0f583e5c2db8f7
WhenYouSadCallMe <actions@users.noreply.github.com>
2026-05-31 18:33:03 +0800

    Release long press away from tap point

--- src/AnClickFakeTouch.m
+++ src/AnClickFakeTouch.m
@@ -30,6 +30,7 @@ static CGPoint AnClickHoldPoint = {0, 0};
 static dispatch_source_t AnClickHoldTimer = nil;
 static NSUInteger AnClickHoldGeneration = 0;
 static const CGFloat AnClickHoldJitter = 0.5;
+static const CGFloat AnClickLongPressReleaseOffset = 18.0;
 static const NSTimeInterval AnClickHoldTickInterval = 0.02;
 
 + (void)tapAtPoint:(CGPoint)point {
@@ -42,6 +43,16 @@ static const NSTimeInterval AnClickHoldTickInterval = 0.02;
         [self touchUpAtPoint:point touchId:touchId];
         [self triggerUIKitControlAtPoint:point];
     });
+}
+
++ (CGPoint)longPressReleasePointForPoint:(CGPoint)point {
+    CGRect bounds = UIScreen.mainScreen.bounds;
+    CGFloat dx = (point.x + AnClickLongPressReleaseOffset <= CGRectGetMaxX(bounds)) ? AnClickLongPressReleaseOffset : -AnClickLongPressReleaseOffset;
+    CGFloat dy = (point.y + AnClickLongPressReleaseOffset <= CGRectGetMaxY(bounds)) ? AnClickLongPressReleaseOffset : -AnClickLongPressReleaseOffset;
+    CGPoint releasePoint = CGPointMake(point.x + dx, point.y + dy);
+    releasePoint.x = MIN(MAX(releasePoint.x, CGRectGetMinX(bounds)), CGRectGetMaxX(bounds));
+    releasePoint.y = MIN(MAX(releasePoint.y, CGRectGetMinY(bounds)), CGRectGetMaxY(bounds));
+    return releasePoint;
 }
 
 + (void)doubleTapAtPoint:(CGPoint)point {
@@ -124,8 +135,9 @@ static const NSTimeInterval AnClickHoldTickInterval = 0.02;
     NSInteger touchId = AnClickHoldTouchId;
     AnClickHolding = NO;
     AnClickHoldGeneration++;
-    [self touchMoveAtPoint:CGPointMake(point.x + AnClickHoldJitter, point.y + AnClickHoldJitter) touchId:touchId];
-    [self touchUpAtPoint:point touchId:touchId];
+    CGPoint releasePoint = [self longPressReleasePointForPoint:point];
+    [self touchMoveAtPoint:releasePoint touchId:touchId];
+    [self touchUpAtPoint:releasePoint touchId:touchId];
 }
 
 + (void)cancelHold {
@@ -284,6 +296,9 @@ static const NSTimeInterval AnClickHoldTickInterval = 0.02;
     __block NSTimeInterval lastTimestamp = 0;
     BOOL touchIsDown = NO;
     BOOL gestureActive = NO;
+    CGPoint gestureStartPoint = CGPointZero;
+    NSTimeInterval gestureStartTimestamp = 0;
+    CGFloat maxGestureDistance = 0;
     NSTimeInterval previousTimestamp = 0;
     CGPoint previousPoint = CGPointZero;
     NSInteger previousType = -1;
@@ -323,13 +338,29 @@ static const NSTimeInterval AnClickHoldTickInterval = 0.02;
         if (type == 0) {
             touchIsDown = YES;
             gestureActive = YES;
+            gestureStartPoint = point;
+            gestureStartTimestamp = timestamp;
+            maxGestureDistance = 0;
         } else if (gestureActive) {
+            CGFloat dx = point.x - gestureStartPoint.x;
+            CGFloat dy = point.y - gestureStartPoint.y;
+            maxGestureDistance = MAX(maxGestureDistance, sqrt(dx * dx + dy * dy));
             if (type == 2) {
                 touchIsDown = NO;
                 gestureActive = NO;
             } else if (type == 3) {
                 touchIsDown = NO;
                 gestureActive = NO;
+            }
+        }
+
+        BOOL releaseAwayFromTapPoint = NO;
+        CGPoint playbackReleasePoint = point;
+        if (type == 2) {
+            NSTimeInterval heldDuration = MAX(0, timestamp - gestureStartTimestamp);
+            releaseAwayFromTapPoint = (heldDuration >= 0.5 && maxGestureDistance <= 12.0);
+            if (releaseAwayFromTapPoint) {
+                playbackReleasePoint = [self longPressReleasePointForPoint:point];
             }
         }
 
@@ -340,6 +371,9 @@ static const NSTimeInterval AnClickHoldTickInterval = 0.02;
                 [self touchMoveAtPoint:point touchId:touchId];
             } else if (type == 3) {
                 [self touchCancelAtPoint:point touchId:touchId];
+            } else if (releaseAwayFromTapPoint) {
+                [self touchMoveAtPoint:playbackReleasePoint touchId:touchId];
+                [self touchUpAtPoint:playbackReleasePoint touchId:touchId];
             } else {
                 [self touchUpAtPoint:point touchId:touchId];
             }
