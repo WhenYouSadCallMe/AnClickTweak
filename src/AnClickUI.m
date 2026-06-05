c30b16f376b84be6abfb7dc197e969d3f211ad65
WhenYouSadCallMe <actions@users.noreply.github.com>
2026-06-05 12:23:36 +0800

    Fix dylib build after launcher removal

--- src/AnClickUI.m
+++ src/AnClickUI.m
@@ -9465,7 +9465,7 @@ __attribute__((constructor)) static void AnClickUIInit(void) {
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
             AnClickInstallSpringBoardPhysicalButtonHook();
             AnClickInstallSpringBoardVolumeControlHook();
-        }];
+        });
         return;
     }
 
